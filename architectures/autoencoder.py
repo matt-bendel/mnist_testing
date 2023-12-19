5a8694712304e2b99e6058c7f1b83368922f0cea
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-12-18 23:09:37 -0500

    autoencoder

--- architectures/autoencoder.py
+++ architectures/autoencoder.py
@@ -11,23 +11,25 @@ class Autoencoder(nn.Module):
     def __init__(self, width_factor=1):
         super().__init__()
         width = int(width_factor * 32)
-        layers = [nn.Conv2d(1, width, kernel_size=3, padding=1, stride=1),
-                  nn.LeakyReLU(negative_slope=0.2),
-                  nn.AvgPool2d(2, stride=2, padding=1),
-                  nn.Conv2d(width, width, kernel_size=3, padding=1, stride=1),
-                  nn.LeakyReLU(negative_slope=0.2),
-                  nn.AvgPool2d(2, stride=2, padding=0)]
-        self.encoder = nn.Sequential(*layers)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, width, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.AvgPool2d(2, stride=2, padding=1),
+            nn.Conv2d(width, width, kernel_size=3, padding=1, stride=1),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.AvgPool2d(2, stride=2, padding=0)
+        )
 
         modules = []
-        modules += [[nn.Conv2d(width, width, kernel_size=3, stride=1, padding=1),
-                     nn.LeakyReLU(negative_slope=0.2)]]
-        modules += [[nn.Conv2d(width, width, kernel_size=3, stride=1, padding=1),
-                     nn.LeakyReLU(negative_slope=0.2)]]
-        modules += [[nn.Conv2d(width, 1, kernel_size=3, stride=1, padding=1)]]
-        self.decoders = []
-        for module in modules:
-            self.decoders += [nn.Sequential(*module)]
+        self.decoder_1 = nn.Sequential(
+            nn.Conv2d(width, width, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(negative_slope=0.2)
+        )
+        self.decoder_2 = nn.Sequential(
+            nn.Conv2d(width, width, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(negative_slope=0.2)
+        )
+        self.decoder_3 = nn.Conv2d(width, 1, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x, features=False):
         x = self.encoder(x)
@@ -35,9 +37,10 @@ class Autoencoder(nn.Module):
         if features:
             return x
 
-        for i, decoder in enumerate(self.decoders):
-            x = decoder(x)
-            if i < len(self.decoders) - 1:
-                x = F.interpolate(x, scale_factor=2)
+        x = self.decoder_1(x)
+        x = F.interpolate(x, scale_factor=2)
+        x = self.decoder_2(x)
+        x = F.interpolate(x, scale_factor=2)
+        x = self.decoder_3(x)
 
         return x
