bb61cc4e30ac74ae6fe37c738e54a721bcc30980
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-12-23 15:56:29 -0500

    Updated validation

--- architectures/autoencoder.py
+++ architectures/autoencoder.py
@@ -15,24 +15,24 @@ class Autoencoder(nn.Module):
         super().__init__()
         width = int(width_factor * 32)
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, width, kernel_size=3, padding=1, stride=1),
+            nn.Conv2d(1, 4, kernel_size=3, padding=1, stride=1),
             nn.LeakyReLU(negative_slope=0.2),
             nn.AvgPool2d(2, stride=2, padding=1),
-            nn.Conv2d(width, width, kernel_size=3, padding=1, stride=1),
+            nn.Conv2d(4, 8, kernel_size=3, padding=1, stride=1),
             nn.LeakyReLU(negative_slope=0.2),
             nn.AvgPool2d(2, stride=2, padding=0)
         )
 
         modules = []
         self.decoder_1 = nn.Sequential(
-            nn.Conv2d(width, width, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(8, 8, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(negative_slope=0.2)
         )
         self.decoder_2 = nn.Sequential(
-            nn.Conv2d(width, width, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(8, 4, kernel_size=3, stride=1, padding=1),
             nn.LeakyReLU(negative_slope=0.2)
         )
-        self.decoder_3 = nn.Conv2d(width, 1, kernel_size=3, stride=1, padding=1)
+        self.decoder_3 = nn.Conv2d(4, 1, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x, features=False):
         x = self.encoder(x)
