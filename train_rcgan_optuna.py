95b70cfb9c9951640cbcfb3563f85590609f7e62
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2023-12-30 15:54:54 -0500

    Updated validation

---
+++ train_rcgan_optuna.py
@@ -0,0 +1,74 @@
+import torch
+import yaml
+import types
+import json
+
+import pytorch_lightning as pl
+# TODO: Install Matplotlib, wandb
+
+from pytorch_lightning.callbacks import ModelCheckpoint
+from utils.parse_args import create_arg_parser
+from pytorch_lightning import seed_everything
+from pytorch_lightning.loggers import WandbLogger
+from models.lightning.rcGAN_mac import rcGAN
+from data.lightning.MNISTDataModule import MNISTDataModule
+from models.lightning.rcGAN_mac_w_pc_reg import rcGANWRegOptuna
+import optuna
+from optuna.integration import PyTorchLightningPruningCallback
+
+def load_object(dct):
+    return types.SimpleNamespace(**dct)
+
+
+def objective(trial, cfg, args):
+    # We optimize the number of layers, hidden units in each layer and dropouts.
+    n_layers = trial.suggest_int("n_layers", 1, 3)
+    dropout = trial.suggest_float("dropout", 0.2, 0.5)
+    output_dims = [
+        trial.suggest_int("n_units_l{}".format(i), 4, 128, log=True) for i in range(n_layers)
+    ]
+
+    start_lr = trial.suggest_float("start_lr", 1e-3, 1e-4)
+    beta_pca = trial.suggest_float('beta_pca', 1e-3, 1)
+    patience = trial.suggest_int('lr_patience', 5, 20)
+    lr_step = trial.suggest_float('lr_patience', 0.5, 0.99)
+
+    model = rcGANWRegOptuna(cfg, args.exp_name, start_lr, beta_pca, patience, lr_step)
+    dm = MNISTDataModule()
+
+    trainer = pl.Trainer(accelerator="gpu", strategy='ddp', devices=1, enable_checkpointing=False,
+                         max_epochs=cfg.num_epochs, callbacks=[PyTorchLightningPruningCallback(trial, monitor="cfid")],
+                         num_sanity_val_steps=2, profiler="simple", logger=True, benchmark=False,
+                         log_every_n_steps=10)
+
+    trainer.fit(model, dm)
+
+    return trainer.callback_metrics["cfid"].item()
+
+
+if __name__ == '__main__':
+    torch.set_float32_matmul_precision('medium')
+    args = create_arg_parser().parse_args()
+    seed_everything(0, workers=True)
+
+    print(f"Experiment Name: {args.exp_name}")
+
+    with open('configs/rcgan.yml', 'r') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+        cfg = json.loads(json.dumps(cfg), object_hook=load_object)
+
+    pruner = optuna.pruners.NopPruner()
+
+    study = optuna.create_study(direction="minimize", pruner=pruner)
+    study.optimize(objective, n_trials=100)
+
+    print("Number of finished trials: {}".format(len(study.trials)))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: {}".format(trial.value))
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
