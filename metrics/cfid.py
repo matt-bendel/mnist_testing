928b5c587ee4b073874126ae6be82af001a14e05
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-02-11 01:18:25 -0500

    inception

--- metrics/cfid.py
+++ metrics/cfid.py
@@ -171,9 +171,13 @@ class CFIDMetric:
                     condition_im = y
                     true_im = x
 
-                    img_e = self.image_embedding(self.process_inception(image, 0.1307, 0.3801), features=True)
-                    cond_e = self.condition_embedding(self.process_inception(condition_im, 0.1307, 0.3801), features=True)
-                    true_e = self.image_embedding(self.process_inception(true_im, 0.1307, 0.3801), features=True)
+                    # img_e = self.image_embedding(self.process_inception(image, 0.1307, 0.3801), features=True)
+                    # cond_e = self.condition_embedding(self.process_inception(condition_im, 0.1307, 0.3801), features=True)
+                    # true_e = self.image_embedding(self.process_inception(true_im, 0.1307, 0.3801), features=True)
+
+                    img_e = self.image_embedding(self.process_inception(image, 0.1307, 0.3801))
+                    cond_e = self.condition_embedding(self.process_inception(condition_im, 0.1307, 0.3801))
+                    true_e = self.image_embedding(self.process_inception(true_im, 0.1307, 0.3801))
 
                     if self.cuda:
                         true_embed.append(true_e)
