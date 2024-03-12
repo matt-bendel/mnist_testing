05f7e33e09b1ffb5b675f49f3f7ea0822f300a04
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-03-12 18:28:03 -0400

    ffhw shift

--- test_rcgan_nppc.py
+++ test_rcgan_nppc.py
@@ -78,7 +78,8 @@ if __name__ == '__main__':
 
             for n in range(x.shape[0]):
                 _, S, Vh = torch.linalg.svd(gens_zm[n], full_matrices=False)
-                print(Vh.shape)
+                print(S.shape)
+                Vh = Vh[0:5, :]
 
                 unsqueezed_err = torch.unsqueeze(err[n, :], dim=1)
 
