04a3f4fb98c5ba938acb2eeef92a89e5d9900aae
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-05-21 12:54:22 -0400

    Moved notebook to script

--- plot_mnist_nppc.py
+++ plot_mnist_nppc.py
@@ -83,8 +83,8 @@ class MidpointNormalize(mpl.colors.Normalize):
         x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
         return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
 
-evec1 = 1
-evec2 = 2
+evec1 = 0
+evec2 = 3
 
 fig_count = 0
 with torch.no_grad():
