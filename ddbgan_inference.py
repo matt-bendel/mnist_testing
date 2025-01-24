a1462289949cf06cdccd6f92dd602db4b17434f8
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2025-01-24 15:05:34 -0500

    DDB rcGAN

--- ddbgan_inference.py
+++ ddbgan_inference.py
@@ -113,7 +113,7 @@ if __name__ == '__main__':
     dm.setup()
     test_loader = dm.test_dataloader()
 
-    N = 100
+    N = 5
     delta = 1 / N
     num_samps = 8
     t_steps = (torch.arange(N) + 1) / N
